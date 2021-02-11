090d53dbc5629d90b773f1f081ab7da9f18b3732
Alexander Upton <Alexander99W@ou.edu>
2021-02-11 17:40:41 -0600

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 101);//generate a random number betw/ 1-100
+            int divisableBy3Remainder = randomNbr;
+            bool isDivisibleBy3 = divisableBy3Remainder == 0;
+            
+            if (randomNbr % 3 == 0
+                && randomNbr % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+
+            }
+            else if (randomNbr % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomNbr % 5 == 0 )
+            {
+                Console.WriteLine("Fizz");
+            }
+            
+        }
+    }
+}
